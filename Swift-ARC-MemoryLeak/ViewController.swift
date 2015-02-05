d79c676aa6c151c390a7f0e2799bbfd2849e30b8
Tomas Kraina <tomas@mailcloud.com>
2015-02-05 09:22:47 +0000

    Initial Commit

---
+++ Swift-ARC-MemoryLeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-ARC-MemoryLeak
+//
+//  Created by Tomas Kraina on 05/02/2015.
+//  Copyright (c) 2015 Tom Kraina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
